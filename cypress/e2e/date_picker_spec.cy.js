08d537f3fa5c98fe382d376a2397e375e2b407e0
Nikita <nekityo0@gmail.com>
2024-10-18 13:21:28 +0200

    Update

--- cypress/e2e/date_picker_spec.cy.js
+++ cypress/e2e/date_picker_spec.cy.js
@@ -1,16 +1,18 @@
-describe('Date Picker Test', () => {
+describe('Date Picker and Date And Time Test', () => {
     beforeEach(() => {
         cy.visit('https://demoqa.com/date-picker');
     });
 
-    it('should select February 28, 2013, at 12:00 PM', () => {
+    it('should select February 28, 2013, in Date Picker', () => {
         cy.get('#datePickerMonthYearInput').click();
         cy.get('.react-datepicker__year-select').select('2013');
-        cy.get('.react-datepicker__month-select').select('February');
-        cy.get('.react-datepicker__day').contains('28').click();
-        cy.get('#datePickerMonthYearInput').should('have.value', '01/28/2013'); // Проверка на MM/DD/YYYY
-        
-        cy.get('#dateAndTimePickerInput').clear().type('October 17, 2024 12:00 PM'); // Очистка поля перед вводом
-        cy.get('#dateAndTimePickerInput').should('have.value', 'October 17, 2024 12:00 PM'); // Проверка на корректное значение
+        cy.get('.react-datepicker__month-select').select('1'); // 1 = февраль
+        cy.get('.react-datepicker__day--028:not(.react-datepicker__day--outside-month)').click();
+        cy.get('#datePickerMonthYearInput').should('have.value', '02/28/2013');
+    });
+
+    it('should select February 28, 2013, at 12:00 PM in Date And Time Picker', () => {
+        cy.get('#dateAndTimePickerInput').clear().type('February 28, 2013 12:00 PM');
+        cy.get('#dateAndTimePickerInput').should('have.value', 'February 28, 2013 12:00 PM');
     });
 });
